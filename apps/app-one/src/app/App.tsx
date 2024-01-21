3eb1fc276c771a704e8d06e0d0627d3716b72707
gionny96 <gionathas@gmail.com>
2024-01-21 13:49:15 +0100

    Add new files and update dependencies

---
+++ apps/app-one/src/app/App.tsx
@@ -0,0 +1,12 @@
+export function App() {
+	return (
+		<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+			<h1>
+				<span> Hello there, </span>
+				Welcome <span style={{ color: 'red' }}>app-one</span> 👋
+			</h1>
+		</div>
+	)
+}
+
+export default App
